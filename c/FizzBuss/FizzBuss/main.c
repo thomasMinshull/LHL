187749226cbca2ec46f9e2a3cf0e648ca109675b
Thomas Minshull <thomasminshull@gmail.com>
2016-06-19 16:16:56 -0300

    refactored file structure and answered Assignment2

---
+++ c/FizzBuss/FizzBuss/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  FizzBuss
+//
+//  Created by thomas minshull on 2016-06-19.
+//  Copyright © 2016 thomas minshull. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
