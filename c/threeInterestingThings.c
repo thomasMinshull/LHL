8006313355f869f0ce57d69a04a9ed69e602b61e
Thomas Minshull <thomasminshull@gmail.com>
2016-06-19 11:34:01 -0300

    added three interesting things program

---
+++ c/threeInterestingThings.c
@@ -0,0 +1,7 @@
+#import <stdio.h>
+int main(){ 
+	printf("I used to be a welder\n");
+	printf("I stopped welding to go back to university and study economics where I discoverd programming\n"); 
+	printf("Now I work as a mobile developer!\n");
+}
+
