7492141f9268aa154e575392ff5df452b5f484b8
Thomas Minshull <thomasminshull@gmail.com>
2016-06-22 14:28:02 -0300

    fizz buzz complete

--- c/fizzBuzz/fizzBuzz/main.c
+++ c/fizzBuzz/fizzBuzz/main.c
@@ -9,7 +9,21 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    for ( int x = 1; x < 101; x++ ) {
+        
+        if (x%3 == 0 && x%5 == 0) {
+            printf("FizzBuzz\n");
+        
+        } else if (x%3 == 0) {
+            printf("Fizz\n");
+        
+        } else if (x%5 == 0) {
+            printf("Buzz\n");
+        
+        } else {
+            printf("%d\n", x);
+        }
+    }
     return 0;
 }
