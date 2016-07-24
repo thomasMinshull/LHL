65c9f92f454b0b85960e1b8a3fa3e7d075fc156e
Thomas Minshull <thomasminshull@gmail.com>
2016-07-23 18:14:17 -0700

    loggin in and retrieving basic profile information

--- pingMidTermProject/PingLinkedInLoginTest/PingLinkedInLoginTest/ViewController.m
+++ pingMidTermProject/PingLinkedInLoginTest/PingLinkedInLoginTest/ViewController.m
@@ -22,6 +22,15 @@
     // CheckKeyChain for access token
         // If there is an acess token attemp to login with access token
     
+    
+    /* sudo code
+            NSString tokenFromKeyChain = [Keychain getObjectForKey: KEY];
+            
+            if (tokenFromKeyChain) { */
+               // LISDKAccessToken *token = [LISDKAccessToken LISDKAccessTokenWithSerializedString:tokenFromKeyChain];
+                // LISDKSession *session = [LISDKSessionManager createSessionWithAccessToken:token];
+    
+    
         // If there is not an access token or if the login attempt with the access token fails...
         // login and request new access token
     
@@ -34,11 +43,24 @@
      successBlock:^(NSString *returnState) {
          NSLog(@"%s","success called!");
          LISDKSession *session = [[LISDKSessionManager sharedInstance] session];
+         
+         NSString *url = @"https://api.linkedin.com/v1/people/~";
+         
+         if ([LISDKSessionManager hasValidSession]) {
+             [[LISDKAPIHelper sharedInstance] getRequest:url
+                                                 success:^(LISDKAPIResponse *response) {
+                                                     // do something with response
+                                                     NSLog(@"got user profile: %@", response.data);
+                                                 }
+                                                   error:^(LISDKAPIError *apiError) {
+                                                       // do something with error
+                                                       NSLog(@"Failed to get user profile: %@", apiError);
+                                                   }];
+         }
      }
      errorBlock:^(NSError *error) {
          NSLog(@"%s","error called!");
-     }
-     ];
+     }];
     
     DeeplinkSuccessBlock success = ^(NSString *returnState) {
         NSLog(@"Success with returned state: %@",returnState);
